2a0ec5a48ea47707165f33f5627783d23ac34fc2
Shankar Nathan <shankar.nathan@grabtaxi.com>
2021-12-17 12:18:21 +0800

    save

--- scripts/run.js
+++ scripts/run.js
@@ -8,8 +8,6 @@ async function main() {
     const contractFactory = await hre.ethers.getContractFactory("Y2123");
 
     const uri = "https://gateway.pinata.cloud/ipfs/QmWoaUxvJLvynACkE9KKj3GkgbWQDToqdSmEL97cRynGrf/";
-    //const root1 = '0x4450f575d669ae24697c974018bbbb3db32139532f7d25b71e443d06c2637f01';
-    //const root2 = '0xebecb36a6776ac70a2a6af26f91099730e830d664a0b416ae261fb2732aee002';
 
     // Deploy contract with the correct constructor arguments
     const contract = await contractFactory.deploy(uri);
