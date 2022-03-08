005265ff8e9a19a99e6ec62e2f54d16f9803e74c
Shanq <shanq@Shanqs-MacBook-Pro.local>
2022-03-08 19:24:40 +0800

    save

--- scripts/deploy_clans.js
+++ scripts/deploy_clans.js
@@ -1,22 +1,40 @@
 const { utils } = require("ethers");
+const { ethers } = require("hardhat");
+require("dotenv").config();
+
+const { TEST1_PK, DEPLOY_PK } = process.env;
 
 async function main() {
-  // Get owner/deployer's wallet address
-  //const [owner] = await hre.ethers.getSigners();
-
-  // Get contract that we want to deploy
+  
   const contractFactory = await hre.ethers.getContractFactory("Clans");
-
   const uri = "https://api.y2123.io/asset-clans?id=";
-
-  // Deploy contract with the correct constructor arguments
-  const contract = await contractFactory.deploy(uri, "0x8e3DA90f2f00f979E7F960c4CF627F62E6Da5B43", "0x4088d86cA721f75B75773faa89b6475931Bb8FBf");
-
-  // Wait for this transaction to be mined
+  const overrides = {
+    nonce: 61,
+    //maxPriorityFeePerGas: ethers.BigNumber.from(2000000000),
+    //maxFeePerGas: ethers.BigNumber.from(2000000000),
+  };
+  const contract = await contractFactory.deploy(uri, "0x08dB6FE68EDD5A9f26502f5dE274bAF1573D9222", "0xAd20084e30624F5eB5d2346ea509C35A86E8f9eB", overrides);
   await contract.deployed();
-
-  // Get contract address
   console.log("Contract deployed to:", contract.address);
+  
+  // Override nonce 62 with send ETH
+  /*
+  const deployWallet = new ethers.Wallet(DEPLOY_PK, hre.ethers.provider);
+  const me = "0x8360d6D8A7B00C0564130B0f1FB6e82789415deE";
+  const gasPrice = await hre.ethers.provider.getGasPrice();
+  console.log("gasPrice: ", ethers.utils.formatEther(gasPrice), "eth");
+  const tx = {
+    from: deployWallet.address,
+    to: me,
+    value: ethers.utils.parseUnits("0.01", "ether"),
+    gasPrice: gasPrice.mul(2),
+    gasLimit: ethers.utils.hexlify(10000), // 10 gwei
+    //nonce:hre.ethers.provider.getTransactionCount(deployWallet.address, 'latest'),
+    nonce: 62,
+  }
+  const transaction = await deployWallet.sendTransaction(tx);
+  console.log(transaction);
+  */
 }
 
 main()
