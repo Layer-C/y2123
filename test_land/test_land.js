b386f0f390b778365cadd14caf612d781c8b7e27
Gaurav Upadhyay <11389063+gauravupadhyay1994@users.noreply.github.com>
2022-04-18 19:54:16 +0530

    Update test_land.js

--- test_land/test_land.js
+++ test_land/test_land.js
@@ -39,6 +39,99 @@ describe("Land Contract", function () {
       .to.emit(landContract, "SaleActive")
       .withArgs(true);
   });
+  it("Everthing about Tokens", async () => {
+
+    let contract = await ethers.getContractFactory("Oxygen");
+    oContract = await contract.deploy();
+    await oContract.deployed();
+
+    contract = await ethers.getContractFactory("Land");
+    landContract = await contract.deploy(uri, oContract.address, clansAddress, proxyRegistryAddress);
+    await landContract.deployed();
+
+
+    let totSupply = await landContract.totalSupply();
+    expect(totSupply).to.equal(0);
+
+    const [...tokenOwner] = await landContract.getTokenIDs(landContract.owner());
+    expect(await tokenOwner.length).to.equal(0);
+
+    await oContract.addAdmin(landContract.address);
+    await oContract.addAdmin(landContract.owner());
+    //await oContract.removeAdmin(landContract.owner());
+    await oContract.mint(landContract.owner(), ethers.utils.parseEther("1000.0"));
+    await landContract.paidMint(1);
+
+    const [...tokenOwner1] = await landContract.getTokenIDs(landContract.owner());
+    expect(await tokenOwner1.length).to.equal(1);
+
+    totSupply = await landContract.totalSupply();
+    expect(totSupply).to.equal(tokenOwner1.length);
+
+
+  });
+
+  it("Staking", async () => {
+
+    contract = await ethers.getContractFactory("Y2123");
+    const uri = "https://api.y2123.io/asset?id=";
+    yContract = await contract.deploy(uri);
+    await yContract.deployed();
+
+    contract = await ethers.getContractFactory("Oxygen");
+    oContract = await contract.deploy();
+    await oContract.deployed();
+
+    contract = await ethers.getContractFactory("contracts/Clans.sol:Clans");
+    const clan_uri = "https://api.y2123.io/clan-asset?id=";
+    cContract = await contract.deploy(clan_uri, oContract.address, yContract.address);
+    await cContract.deployed();
+
+    contract = await ethers.getContractFactory("Land");
+    const land_uri = "https://api.y2123.io/land-asset?id="
+    landContract = await contract.deploy(land_uri, oContract.address, cContract.address, proxyRegistryAddress);
+    await landContract.deployed();
+
+    await oContract.addAdmin(landContract.address);
+    await oContract.addAdmin(landContract.owner());
+
+    await landContract.addContract(yContract.address);
+
+    await oContract.mint(landContract.owner(), ethers.utils.parseEther("1000.0"));
+    await landContract.paidMint(1);
+
+    await oContract.mint(yContract.owner(), ethers.utils.parseEther("1000.0"));
+    // the code was showing some error will do it tomorrow.
+
+    //await yContract.paidMint(3, []);
+
+    //await landContract.stakedByOwner(yContract.address, yContract.owner);
+
+
+  });
+
+  it("Oxygen in Land", async () => {
+    let contract = await ethers.getContractFactory("Oxygen");
+    oContract = await contract.deploy();
+    await oContract.deployed();
+
+    contract = await ethers.getContractFactory("Land");
+    landContract = await contract.deploy(uri, oContract.address, clansAddress, proxyRegistryAddress);
+    await landContract.deployed();
+
+
+    let nextLevel = await landContract.nextLevelTankPrice(landContract.owner());
+
+    //await oContract.addAdmin(landContract.address);
+    //await oContract.addAdmin(landContract.owner());
+    //await oContract.mint(landContract.owner(), ethers.utils.parseEther("1000.0"));
+
+    //await landContract.upgradeTank();
+    //let tankLevel = await landContract.tankLevelOfOwner(landContract.owner());
+    //expect(tankLevel).to.equal(nextLevel);
+
+
+  });
 });
 
 
